76ab2e7faeeb59263280cb56555704c10bc32f0d
Mahesh <Mahesh@MaheshBabu.attlocal.net>
2021-06-21 15:19:48 -0500

    Commit Test

---
+++ test2/src/tests/TestProject_01.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class TestProject_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Test2 Repository");
+	}
+
+}
